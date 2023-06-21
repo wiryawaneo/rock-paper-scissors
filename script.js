e9fcfa552c6bfbbc18028082aff551c3c969a8c7
wiryawaneo <35023905+wiryawaneo@users.noreply.github.com>
2023-06-21 23:19:47 +0800

    add function to compare user input and computer

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
 const choices = ["rock", "paper", "scissors"];
 
-function getComputerChoice(options) {
-  return console.log(options[Math.floor(Math.random() * options.length)]);
+function getComputerChoice() {
+  const choices = ["rock", "paper", "scissors"];
+  return choices[Math.floor(Math.random() * choices.length)];
 }
 
-getComputerChoice(choices);
+console.log(getComputerChoice());
 
 let playerSelection = prompt("Rock, paper or scissors?");
+
+function playRound(playerChoice, computerChoice) {
+  if (playerChoice === computerChoice) {
+    return "Draw! Both are " + playerChoice;
+  } else if (playerChoice === "rock" && computerChoice === "scissors") {
+    return "Win!" + playerChoice + " beats " + computerChoice;
+  } else if (playerChoice === "paper" && computerChoice === "rock") {
+    return "Win!" + playerChoice + " beats " + computerChoice;
+  } else if (playerChoice === "scissors" && computerChoice === "paper") {
+    return "Win!" + playerChoice + " beats " + computerChoice;
+  } else if (playerChoice === "rock" && computerChoice === "paper") {
+    return "Lost!" + computerChoice + " beats " + playerChoice;
+  } else if (playerChoice === "paper" && computerChoice === "scissors") {
+    return "Lost!" + computerChoice + " beats " + playerChoice;
+  } else if (playerChoice === "scissors" && computerChoice === "rock") {
+    return "Lost!" + computerChoice + " beats " + playerChoice;
+  } else if (
+    playerChoice != "rock" ||
+    playerChoice != "paper" ||
+    playerChoice != "scissors"
+  ) {
+    return "Player has sent a wrong input";
+  }
+}
 
 if (
   playerSelection.toLowerCase() === "rock" ||
   playerSelection.toLowerCase() === "scissors" ||
   playerSelection.toLowerCase() === "paper"
 ) {
+  console.log(playRound(playerSelection, getComputerChoice()));
   console.log(playerSelection.toLowerCase());
 } else {
   console.log("wrong input!");
-  playerSelection = "";
 }
