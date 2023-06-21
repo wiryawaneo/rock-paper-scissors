99f013b2d2a2b580ad1b0782ad21f5ed21c99898
wiryawaneo <35023905+wiryawaneo@users.noreply.github.com>
2023-06-21 23:01:45 +0800

    add function to receive user selection

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-const choices = ['rock', 'paper', 'scissors']
+const choices = ["rock", "paper", "scissors"];
 
 function getComputerChoice(options) {
-   return console.log(options[Math.floor(Math.random()*options.length)])
+  return console.log(options[Math.floor(Math.random() * options.length)]);
 }
 
-getComputerChoice(choices)
+getComputerChoice(choices);
+
+let playerSelection = prompt("Rock, paper or scissors?");
+
+if (
+  playerSelection.toLowerCase() === "rock" ||
+  playerSelection.toLowerCase() === "scissors" ||
+  playerSelection.toLowerCase() === "paper"
+) {
+  console.log(playerSelection.toLowerCase());
+} else {
+  console.log("wrong input!");
+  playerSelection = "";
+}
